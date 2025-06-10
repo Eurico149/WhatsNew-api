f86e3745d759ae1d7b6b78c187145253c4685cc0
Eurico149 <euricogabriel149@gmail.com>
2025-06-10 11:57:03 -0300

    Initial commit

---
+++ src/main/java/info/euricopersonal/whatsnew/WhatsnewApplication.java
@@ -0,0 +1,13 @@
+package info.euricopersonal.whatsnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsnewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/euricopersonal/whatsnew/WhatsnewApplicationTests.java
@@ -0,0 +1,13 @@
+package info.euricopersonal.whatsnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
